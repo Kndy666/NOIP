92eb0bedc7df3f039ede35fc8bdd8019d79c38c3
Kndy666 <kndy_666@163.com>
2019-01-17 20:04:31 +0800

    Update2019年01月17日20时04分30秒

--- 02/HomeWork/pen/pen.cpp
+++ 02/HomeWork/pen/pen.cpp
@@ -13,6 +13,12 @@ int main()
   int a, b;
   cin >> a >> b;
   
+  if(a > 10000 || b > 9)
+    {
+      cout << "0" << endl; //表示数据范围出错
+      return 1;
+    }
+
   int money = a * 10 + b; //将输入价格换算为角
   
   cout << money / PRICE << " " <<  money % PRICE << endl;
